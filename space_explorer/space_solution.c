532f7810e1d7846af4b19f68d7ed85eeb6be6e8d
Arnaud Michel <87248697+MrArnaudMichel@users.noreply.github.com>
2024-03-19 14:42:27 +0000

    v1 exploring

--- space_explorer/space_solution.c
+++ space_explorer/space_solution.c
@@ -1,13 +1,50 @@
+#include <stdbool.h>
+
 #include "space_explorer.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
-ShipAction space_hop(unsigned int crt_planet,
-                   unsigned int *connections,
-                   int num_connections,
-                   double distance_from_mixer,
-                   void *ship_state)
-{
-    /* your solution here */
+typedef struct ShipState {
+	unsigned int* visited_planets;
+	int num_visited_planets;
+	double last_distance_from_mixer;
+	unsigned int last_planet;
+} ShipState;
+
+ShipAction space_hop(unsigned int crt_planet, unsigned int* connections,
+                     int num_connections,
+                     double distance_from_mixer,
+                     void* ship_state) {
+	ShipState* state;
+	if (ship_state == NULL) {
+		state = malloc(sizeof(ShipState));
+		state->visited_planets = malloc(sizeof(unsigned int));
+		state->visited_planets[0] = crt_planet;
+		state->num_visited_planets = 1;
+	} else {
+		state = (ShipState *) ship_state;
+		state->num_visited_planets++;
+		state->visited_planets = realloc(state->visited_planets, sizeof(unsigned int) * state->num_visited_planets);
+		state->visited_planets[state->num_visited_planets - 1] = crt_planet;
+	}
+	ShipAction action;
+	action.ship_state = state;
+	if (distance_from_mixer > state->last_distance_from_mixer) {
+		state->last_distance_from_mixer = distance_from_mixer;
+		state->last_planet = crt_planet;
+		action.next_planet = crt_planet;
+		printf("Returning to planet %d\n", crt_planet);
+	}
+	else {
+		action.next_planet = RAND_PLANET;
+		for (int i = 0; i < num_connections; i++) {
+			if (connections[i] != state->last_planet) {
+				action.next_planet = connections[i];
+				break;
+			}
+		}
+	}
+
+	return action;
 }
