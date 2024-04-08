f5e0a807205f5cf0b980f5767835a70b3e846a3d
Arnaud Michel <87248697+MrArnaudMichel@users.noreply.github.com>
2024-04-08 14:28:45 +0100

    V1.2: 56 hops

--- space_explorer/space_solution.c
+++ space_explorer/space_solution.c
@@ -5,69 +5,60 @@
 #include <string.h>
 #include <stdio.h>
 
-typedef struct {
-	unsigned int planet;
-	unsigned int* connections;
-	int num_connections;
-	double distance_from_mixer;
-	bool visited;
-	bool blocked;
-} SpaceHopState;
+typedef struct PlanetData{
+    unsigned int planet_id;
+    double distance_from_mixer;
+    unsigned int* connections;
+    int num_connections;
+    bool visited;
+}PlanetData;
 
-typedef struct {
-	SpaceHopState* path;
-	int path_length;
-} ShipState;
+typedef struct VisitedPlanets{
+    PlanetData* planets;
+    unsigned int number_planets_visited;
+}VisitedPlanets;
 
-void add_planet_to_path(ShipState* state, unsigned int crt_planet, unsigned int* connections, int num_connections, double distance_from_mixer) {
-    state->path = realloc(state->path, (state->path_length + 1) * sizeof(SpaceHopState));
-    state->path[state->path_length].planet = crt_planet;
-    state->path[state->path_length].connections = malloc(sizeof(unsigned int) * num_connections);
-    memcpy(state->path[state->path_length].connections, connections, sizeof(unsigned int) * num_connections);
-    state->path[state->path_length].num_connections = num_connections;
-    state->path[state->path_length].distance_from_mixer = distance_from_mixer;
-    state->path[state->path_length].visited = false;
-    state->path[state->path_length].blocked = false;
-    state->path_length++;
+typedef struct ShipState{
+    VisitedPlanets* visitedPlanets;
+}ShipState;
+
+void add_planet(unsigned int crt_planet, unsigned int* connections,
+                int num_connections,
+                double distance_from_mixer,
+                ShipState* state){
+    unsigned int index_planet = state->visitedPlanets->number_planets_visited++;
+    state->visitedPlanets->planets = realloc(state->visitedPlanets->planets,
+                                             (index_planet + 1) * sizeof(PlanetData));
+    state->visitedPlanets->planets[index_planet].planet_id = crt_planet;
+    state->visitedPlanets->planets[index_planet].distance_from_mixer = distance_from_mixer;
+    state->visitedPlanets->planets[index_planet].num_connections = num_connections;
+
+    state->visitedPlanets->planets[index_planet].connections = malloc(num_connections * sizeof(unsigned int));
+    memcpy(state->visitedPlanets->planets[index_planet].connections, connections,
+           num_connections * sizeof(unsigned int));
+
+    state->visitedPlanets->number_planets_visited++;
 }
+
 
 ShipAction space_hop(unsigned int crt_planet, unsigned int* connections,
                      int num_connections,
                      double distance_from_mixer,
                      void* ship_state) {
-	ShipAction action;
-	ShipState* state = ship_state;
-	if (state == NULL) {
-		state = malloc(sizeof(ship_state));
-		state->path = malloc(sizeof(SpaceHopState));
-		state->path_length = 0;
-		state->path[state->path_length - 1].distance_from_mixer = RAND_MAX;
-	}
-	bool blocked = distance_from_mixer - state->path[state->path_length - 1].distance_from_mixer > 0;
-	printf("Past distance %f Actual %f result %f\n", state->path[state->path_length - 1].distance_from_mixer, distance_from_mixer, distance_from_mixer - state->path[state->path_length - 1].distance_from_mixer);
-	bool visited = false;
-	unsigned int next_planet = RAND_PLANET;
-	for (int i = 0; i < num_connections; i++) {
-		if (blocked) {
-			next_planet = state->path[state->path_length - 1].planet;
-			printf("Blocked\n");
-			break;
-		}
-		for (int j = 0; j < state->path_length; ++j) {
-			if (connections[i] == state->path[j].planet) {
-				visited = true;
-				break;
-			}
-		}
-		if (!visited) {
-			next_planet = connections[i];
-			break;
-		}
-	}
-
-	add_planet_to_path(state, next_planet, connections, num_connections, distance_from_mixer);
-	action.ship_state = state;
-	action.next_planet = next_planet;
-
+    ShipAction action;
+    ShipState* state = ship_state;
+    if (state == NULL){
+        // Initialisation of ship_state
+        state = (ShipState*)malloc(sizeof(ShipState));
+        state->visitedPlanets = (VisitedPlanets*)malloc(sizeof(VisitedPlanets));
+        state->visitedPlanets->number_planets_visited = 0;
+        state->visitedPlanets->planets = malloc(sizeof(PlanetData));
+        add_planet(crt_planet, connections, num_connections, distance_from_mixer, state);
+        action.next_planet = RAND_PLANET;
+        return action;
+    }
+    printf("Distance: %f\n", distance_from_mixer);
+	action.next_planet = RAND_PLANET;
+    action.ship_state = 0;
 	return action;
 }
